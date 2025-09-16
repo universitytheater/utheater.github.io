5242cdb92ec1a301b1b5dc63b8429b2a4da4258a
willrehmus <willrehmus@gmail.com>
2025-09-16 17:18:59 -0500

    Change anchor system & comment bylaw js

--- assets/js/bylaws.js
+++ assets/js/bylaws.js
@@ -1,54 +1,55 @@
+// Please note that all comments on this code were written after the fact by someone who did not write the code and does not know js.
+// I did my best to understand what was going on and the comments are a result of that. Do not take them as gospel. - Will
+
 var testObj = {};
 (function(testObj) {
-  var all = document.getElementById("all-bylaws");
-  let glob_ctr = 0;
-  processNode(all);
-  function processNode(node) {
-    const nodeRegex = /\s((?:VI{1,3}|I?V|I{1,3})\.[A-Z](?:\.[a-zA-Z1-9\.]*[a-zA-Z1-9])?)/g;
-    for (let [e, subnode] of node.childNodes.entries()) {
+  var all = document.getElementById("all-bylaws"); // create tree of all html(?) elements in the bylaw page
+  let glob_ctr = 0; // this variable gets incremented but never used
+  processNode(all); // call the following function on the structure of all elements
+
+  function processNode(node) { // open processNode function definition
+    // processNode recursively searches for any text matching the format of a section name and turns them into links to that section.
+    const nodeRegex = /\s((?:VI{1,3}|I?V|I{1,3})\.[A-Z](?:\.[a-zA-Z1-9\.]*[a-zA-Z1-9])?)/g; // define regex for the structure of a section name (i.e. RomanNumeral.CapitalLetter.AlphaNumericCharacter.AlphaNumericCharacter or some subset of that)
+    for (let [e, subnode] of node.childNodes.entries()) { // open loop through subnodes
       if (subnode.nodeType == Node.TEXT_NODE) {
-        if (nodeRegex.test(subnode.textContent)) {
+        if (nodeRegex.test(subnode.textContent)) { // filter out everything except elements containing text matching the regex defined above
           nodeRegex.lastIndex = 0;
           var re = nodeRegex,
               str = subnode.textContent,
               offs = 0;
-          while ((match = re.exec(str)) != null) {
-            innode = subnode.splitText(match.index + 1 - offs)
-            subnode = innode.splitText(match[1].length)
-            offs = match.index + match[0].length;
+          while ((match = re.exec(str)) != null) { // loop through all instances where text matches the regex. var match is an array containing 
+            innode = subnode.splitText(match.index + 1 - offs) // splits the node of the element into two, one before and one after the beginning of the matching text. var innode is the node including the matching text and any after it. 
+            subnode = innode.splitText(match[1].length) // splits the newly created node into two, one of just the matching text and one of everything after it. var subnode is the node of the text after. This also seems to modify innode to only include the regex-matching section.
+            offs = match.index + match[0].length; // set the offset so that it will work for future iterations
             // console.log(match[1]);
-            ref_node = findNode(match[1]);
-            rep_innode = document.createElement("a")
-            rep_innode.textContent = match[1];
-            if (ref_node == null) {
+            ref_node = findNode(match[1]); // call a function defined below on the string that matches the regex. Return the node that the regex points to (the target section)
+            rep_innode = document.createElement("a") // initialize a replacement node for the regex-matching node
+            rep_innode.textContent = match[1]; 
+            if (ref_node == null) { // if the regex-matching text does not correspond to a real section, make it an invalid ref 
               rep_innode.classList.add("invalid-ref");
             } else {
-              if (ref_node.firstElementChild && ref_node.firstElementChild.nodeName[0] == "H") {
+              if (ref_node.firstElementChild && ref_node.firstElementChild.nodeName[0] == "H") { // if targeted section has a title, add that title to the target id
                 rep_innode.dataset.targetSection = ref_node.firstElementChild.textContent
               }
-              rep_innode.classList.add("valid-ref");
-              if (!ref_node.id) {
+              rep_innode.classList.add("valid-ref"); // give the replacement node the css class "valid-ref"
+              if (!ref_node.id) { // assign the target section an id and give it the "anchor" class
                 ref_node.id = match[1].replaceAll(".", "-");
-                ref_node.classList.add("anchor");
-                glob_ctr = glob_ctr + 1;
+                glob_ctr = glob_ctr + 1; // increment mystery variable
               }
-              rep_innode.href = "#" + ref_node.id;
+              rep_innode.href = "#" + ref_node.id; // assign the replacement node an href to the target node
             }
-            innode.parentElement.replaceChild(rep_innode,innode);
+            innode.parentElement.replaceChild(rep_innode,innode); // replace innode (the regex-matching text) with rep_innode (the regex-matching text, plus the link to the target and other relevant meta info)
           }
-          // for (match of subnode.textContent.matchAll(nodeRegex)) {
-            //   console.log(match[1]);
-            //   console.log(findNode(match[1]));
-            // }
           }
-        } else {
+        } else { // if the node was not of type TEXT_NODE, run processNode on it and its subnodes
           processNode(subnode);
         }
-      }
-    }
+      } // close loop through subnodes
+    } // close processNode function definition
 
     testObj.findNode = findNode;
-    function findNode(str) {
+    function findNode(str) { // open findNode function definition
+      // findNode returns the node with id matching the string passed to the function
       var segments = str.split(".");
       var lv2 = segments.shift();
       var lv3 = segments.shift();
